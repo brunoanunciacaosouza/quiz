f521d06460faa30bdf675787f21b0e316104fac7
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-05-03 19:42:34 -0300

    context setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useGlobalContext } from './context/context';
+
 import SetupForm from './components/SetupForm';
 import Loading from './components/Loading';
 import Modal from './components/Modal';
@@ -5,7 +7,21 @@ import Modal from './components/Modal';
 import './App.css';
 
 const App = () => {
-  return <div>App</div>;
+  const { waiting, loading, questions, index, correct } = useGlobalContext();
+
+  if (waiting) {
+    return <SetupForm />;
+  }
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  return (
+    <main>
+      <h1>quiz app</h1>
+    </main>
+  );
 };
 
 export default App;
